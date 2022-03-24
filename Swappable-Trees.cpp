913f1c6dd7fbc725435e65af9449650bef13224a
k-unal <87703732+k-unal@users.noreply.github.com>
2022-03-25 02:36:38 +0530

    Add files via upload

---
+++ Swappable-Trees.cpp
@@ -0,0 +1,11 @@
+bool solve(Tree* root0, Tree* root1) {
+    if (!root0 && !root1){
+    return true;
+}
+    if (!root0 || !root1 || root0->val!=root1->val){
+    return false;
+    }
+    return solve(root0->left,root1->left) && solve(root0->right,root1->right)||
+    solve(root0->left,root1->right) && solve(root0->right,root1->left);
+
+}
